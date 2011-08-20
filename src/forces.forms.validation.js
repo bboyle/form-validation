e52e0d61e9ef32591f4ee483e539a13114df94a7
Ben Boyle <benjamins.boyle@gmail.com>
2011-08-20 20:28:15 +1000

    framework with empty script

---
+++ src/forces.forms.validation.js
@@ -0,0 +1,18 @@
+/*
+	forces validation
+
+	UI for form validation
+	requires HTML5 constraint validaton API (recommended forces.html5.constraintValidationAPI.js)
+
+*/
+
+if ( jQuery !== "undefined" ) {
+(function( $ ){
+	"use strict";
+
+
+	// TODO
+
+
+}( jQuery ));
+}
